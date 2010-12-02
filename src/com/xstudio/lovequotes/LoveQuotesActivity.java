f10c0cdd0f9baba2787d478c7fce7263fc5f438e
Amiruddin Nagri <anagri@thoughtworks.com>
2010-12-02 23:24:58 +0530

    [Amir/Abhinav] added sms intent

--- src/com/xstudio/lovequotes/LoveQuotesActivity.java
+++ src/com/xstudio/lovequotes/LoveQuotesActivity.java
@@ -1,13 +1,24 @@
 package com.xstudio.lovequotes;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoveQuotesActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	public void sendQuote(View view) {
+		Uri smsUri = Uri.parse("tel:9980141980");
+		Intent intent = new Intent(Intent.ACTION_VIEW, smsUri);
+		intent.putExtra("sms_body", "shenrenkui");
+		intent.setType("vnd.android-dir/mms-sms");
+		startActivity(intent);
+	}
 }
